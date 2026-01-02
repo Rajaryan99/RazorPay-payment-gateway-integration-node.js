1c521fe03e0ee5a163add204fcc72300002dd192
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-02 13:49:21 +0530

    created payment gateway

--- server/app.js
+++ server/app.js
@@ -1,10 +1,42 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors"
+import Razerpay  from 'razorpay';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', (req,res) => {
     res.send(" Hello Chaman chodu");
+});
+
+app.post('/order', async (req, res) => {
+
+
+    try {
+         const razerpay = new Razerpay({
+        key_id: process.env.TEST_API_KEY,
+        key_secret: process.env.TEST_SECRET_KEY
+
+    });
+
+    const options = req.body;
+    const order = await razerpay.orders.create(options);
+    console.log(order);
+    res.send(order)
+
+    if(!order){
+        return res.status(500).send("error")
+    }
+        
+    } catch (error) {
+        console.log(error)
+    }
+   
 })
 
 app.listen(process.env.PORT, () => {
