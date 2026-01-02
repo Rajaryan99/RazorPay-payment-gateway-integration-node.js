55480a28c20541686c46728a2a960918148eb888
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-02 14:04:02 +0530

    added Product component

---
+++ client/src/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div>
+      <h2>Tshirt</h2>
+      <p>Solid black Cotton Tshirt</p>
+      <img src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Tshirt Image" style={{height: "400px"}} />
+      <br />
+      <button>Pay</button>
+    </div>
+  )
+}
